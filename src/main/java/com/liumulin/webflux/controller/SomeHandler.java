2b79fa55b616b42310e12f4c763e3549c1fb4eeb
liuqiang <liu1142865396@gmail.com>
2021-09-23 18:42:19 +0800

    update

--- src/main/java/com/liumulin/webflux/controller/SomeHandler.java
+++ src/main/java/com/liumulin/webflux/controller/SomeHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -47,13 +48,15 @@ public class SomeHandler {
     // 而 Flux 表示，处理器返回的数据为 0-多个
     @GetMapping("/flux")
     public Flux<String> fluxDemo(@RequestParam String[] hobby) {
-        return Flux.fromArray(hobby);
+        Flux<String> stringFlux = Flux.fromArray(hobby);
+        return stringFlux;
     }
 
     // 集合转 Flux
     @GetMapping("/flux2")
     public Flux<String> fluxDemo2(@RequestParam List<String> hobby) {
-        return Flux.fromStream(hobby.stream());
+        Flux<String> stringFlux = Flux.fromStream(hobby.stream());
+        return stringFlux;
     }
 
     // Flux 底层不会阻塞处理器执行
@@ -71,6 +74,32 @@ public class SomeHandler {
         }));
         log.info("flux-stop");
         return flux;
+    }
+
+    @GetMapping("/flux4")
+    public Flux<Tuple2<String, String>> fluxDemo4(@RequestParam List<String> hobby) {
+        log.info("flux-start");
+        Flux<String> flux = Flux.fromStream(hobby.stream().map(i -> {
+            // 暂停一会儿线程
+            try {
+                TimeUnit.SECONDS.sleep(4);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return " hobby:" + i;
+        }));
+        Flux<String> flux2 = Flux.fromStream(hobby.stream().map(i -> {
+            // 暂停一会儿线程
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return " hobby:" + i;
+        }));
+        Flux<Tuple2<String, String>> flux3 = flux.zipWith(flux2);
+        log.info("flux-stop");
+        return flux3;
     }
 
 
