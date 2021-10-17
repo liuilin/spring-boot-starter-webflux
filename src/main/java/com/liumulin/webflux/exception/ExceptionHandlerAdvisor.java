79389c20dacf274598535e88713970b57cb45ce5
liuqiang <liu1142865396@gmail.com>
2021-10-17 22:39:50 +0800

    update

--- src/main/java/com/liumulin/webflux/exception/ExceptionHandlerAdvisor.java
+++ src/main/java/com/liumulin/webflux/exception/ExceptionHandlerAdvisor.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.support.WebExchangeBindException;
  */
 @ControllerAdvice
 public class ExceptionHandlerAdvisor {
+
     @ExceptionHandler
     public ResponseEntity<String> exHandler(WebExchangeBindException ex) {
         return new ResponseEntity<>(exToStr(ex), HttpStatus.BAD_REQUEST);
